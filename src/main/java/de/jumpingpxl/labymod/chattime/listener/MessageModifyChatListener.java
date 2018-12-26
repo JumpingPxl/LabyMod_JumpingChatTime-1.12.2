07375ab1a02cc1a1561aa0720eb8dc2800fcd171
JumpingPxl <jumpingpxl@gmail.com>
2018-12-26 14:55:48 +0100

    initial commit

---
+++ src/main/java/de/jumpingpxl/labymod/chattime/listener/MessageModifyChatListener.java
@@ -0,0 +1,39 @@
+package de.jumpingpxl.labymod.chattime.listener;
+
+import de.jumpingpxl.labymod.chattime.JumpingAddon;
+import net.labymod.api.events.MessageModifyChatEvent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Nico (JumpingPxl) Middendorf
+ * @date 21.12.2018
+ */
+
+public class MessageModifyChatListener implements MessageModifyChatEvent {
+
+	private JumpingAddon jumpingAddon;
+
+	public MessageModifyChatListener(JumpingAddon jumpingAddon) {
+		this.jumpingAddon = jumpingAddon;
+	}
+
+	@Override
+	public Object onModifyChatMessage(Object object) {
+		if (!jumpingAddon.getSettings().isEnabledChatTime())
+			return object;
+		String chatTimePrefix = "";
+		try {
+			chatTimePrefix = jumpingAddon.getSettings().translateAlternateColorCodes('&', jumpingAddon.getSettings().
+					getChatTimePrefix()).replace("%time%", new SimpleDateFormat(jumpingAddon.getSettings()
+					.stripColor('&', jumpingAddon.getSettings().getChatTimeFormat()))
+					.format(new Date(System.currentTimeMillis())));
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		return new TextComponentString(chatTimePrefix).appendSibling((ITextComponent) object);
+	}
+}
