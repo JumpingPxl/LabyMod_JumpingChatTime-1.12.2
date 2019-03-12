cbfdc5e66dd71488c2bae2509c63f5c946e6fa9c
JumpingPxl <jumpingpxl@gmail.com>
2019-03-12 17:46:49 +0100

    added options to hover and change the position of the time

--- src/main/java/de/jumpingpxl/labymod/chattime/listener/MessageModifyChatListener.java
+++ src/main/java/de/jumpingpxl/labymod/chattime/listener/MessageModifyChatListener.java
@@ -3,7 +3,9 @@ package de.jumpingpxl.labymod.chattime.listener;
 import de.jumpingpxl.labymod.chattime.JumpingAddon;
 import net.labymod.api.events.MessageModifyChatEvent;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.event.HoverEvent;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,15 +27,23 @@ public class MessageModifyChatListener implements MessageModifyChatEvent {
 	public Object onModifyChatMessage(Object object) {
 		if (!jumpingAddon.getSettings().isEnabledChatTime())
 			return object;
-		String chatTimePrefix = "";
+		String time = "";
 		try {
-			chatTimePrefix = jumpingAddon.getSettings().translateAlternateColorCodes('&', jumpingAddon.getSettings().
-					getChatTimePrefix()).replace("%time%", new SimpleDateFormat(jumpingAddon.getSettings()
-					.stripColor('&', jumpingAddon.getSettings().getChatTimeFormat()))
-					.format(new Date(System.currentTimeMillis())));
+			time = new SimpleDateFormat(jumpingAddon.getSettings().stripColor('&',
+					jumpingAddon.getSettings().getChatTimeFormat())).format(new Date(System.currentTimeMillis()));
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
-		return new TextComponentString(chatTimePrefix).appendSibling((ITextComponent) object);
+		TextComponentString chatComponent = new TextComponentString(jumpingAddon.getSettings().
+				translateAlternateColorCodes('&', jumpingAddon.getSettings().getChatTimeStyle()).replace("%time%", time));
+		if (jumpingAddon.getSettings().isEnabledHover())
+			chatComponent.setStyle(new Style().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+					new TextComponentString(jumpingAddon.getSettings().translateAlternateColorCodes('&',
+					jumpingAddon.getSettings().getHoverStyle()).replace("%time%", time)))));
+		if (jumpingAddon.getSettings().isBeforeMessage())
+			return new TextComponentString("").setStyle(new Style()).appendSibling(chatComponent).
+					appendSibling((ITextComponent) object);
+		else
+			return ((ITextComponent) object).appendSibling(chatComponent);
 	}
 }
